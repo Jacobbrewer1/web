945c80bf8ef675d9cfca50d453fbd3f15476dcc4
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-04-09 15:12:59 +0100

    nolint

--- cache/hash_bucket.go
+++ cache/hash_bucket.go
@@ -1,5 +1,5 @@
 package cache
 
-type HashBucket interface {
+type HashBucket interface { // nolint:iface // Used outside the package and allows for future different caches to take to the interface
 	InBucket(key string) bool
 }
