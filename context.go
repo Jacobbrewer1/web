81cbbff15f6075bab157f5f9ef82f00930f3cdeb
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-04-17 15:26:35 +0100

    moving context getter to primary package

---
+++ context.go
@@ -0,0 +1,13 @@
+package web
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// CoreContext returns a context that is cancelled when an interrupt signal is received.
+var CoreContext = func() (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+}
