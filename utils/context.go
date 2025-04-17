c8e7601f3efa2ed5b59a3e99ac94daab5223224b
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-04-17 15:25:22 +0100

    refactoring the base of the context for the app

--- utils/context.go
+++ utils/context.go
@@ -2,23 +2,12 @@ package utils
 
 import (
 	"context"
-	"log/slog"
 	"os"
 	"os/signal"
 	"syscall"
-
-	"github.com/jacobbrewer1/web/logging"
 )
 
-// GetInterruptedContext returns a context that is cancelled when an interrupt signal is received.
-func GetInterruptedContext(l *slog.Logger) (context.Context, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		sig := make(chan os.Signal, 1)
-		signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
-		got := <-sig
-		l.Info("Received signal, shutting down", slog.String(logging.KeySignal, got.String()))
-		cancel()
-	}()
-	return ctx, cancel
+// CoreContext returns a context that is cancelled when an interrupt signal is received.
+var CoreContext = func() (context.Context, context.CancelFunc) {
+	return signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 }
