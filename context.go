d54af6a4226b2fc99d67760bccf1cc8673b7a516
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-04-17 15:29:40 +0100

    docs

--- context.go
+++ context.go
@@ -8,6 +8,9 @@ import (
 )
 
 // CoreContext returns a context that is cancelled when an interrupt signal is received.
+//
+// Note: This is not a named function to allow for overriding by users of the package who may want to use a different
+// core context. This is useful for testing or if the user wants to use a different signal or context.
 var CoreContext = func() (context.Context, context.CancelFunc) {
 	return signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 }
