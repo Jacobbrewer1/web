c48a26a86fac519144503abb4d0e77c876a0e229
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-05-01 09:37:30 +0100

    removing redundant comment to prevent linter

--- cache/hash_bucket.go
+++ cache/hash_bucket.go
@@ -1,7 +1,7 @@
 package cache
 
 // HashBucket is an interface that defines a method to check if a given key belongs to the current bucket.
-type HashBucket interface { // nolint:iface // Used outside the package and allows for future different caches to take to the interface
+type HashBucket interface {
 	// InBucket checks if the provided key belongs to the current bucket.
 	InBucket(key string) bool
 }
